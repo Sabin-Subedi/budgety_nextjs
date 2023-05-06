001699b2b5cb0aa5d43eb5188190be75fec4d21c
Sabin Subedi <sabinsubedi125@gmail.com>
2023-05-06 17:37:42 +0545

    add the form and zod validation schema

---
+++ src/Form/Form.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { ZodSchema } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+interface FormProps {
+  validationSchema: ZodSchema;
+  onSubmit?: (data: FieldValues) => void;
+  children: React.ReactNode | React.ReactNode[];
+}
+const defaultSubmit = (data: FieldValues) => console.log("Form response", data);
+
+function Form({
+  validationSchema,
+  onSubmit = defaultSubmit,
+  children,
+}: FormProps) {
+  const { register, handleSubmit, reset, formState } = useForm({
+    resolver: zodResolver(validationSchema),
+  });
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      onReset={() => {
+        reset();
+      }}
+    ></form>
+  );
+}
+
+export default Form;
